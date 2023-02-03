cdc437495cec09bfb781b8aa09dc022eabc05c6c
Karim Ait-Allaoua <kaitallaoua@gmail.com>
2023-02-03 16:24:32 -0600

    First working script

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # gmr-civ5-turn-notif
-Notify discord users when its their turn in giant multiplayer robot (civ5).
+Notify discord users when its their turn in giant multiplayer robot (civ5). This code was tested on python 3.10.4. Currently, it does not support opt-out for specific users if they wish to not be mentioned, though it wouldn't be too hard to add.
+
+# How to use
+1. Clone the repo
+2. Install requirements: `pip install -r requirements.txt`
+3. Rename `sample.config.json` to `config.json` and modify the entries as following
+
+| `webhook_url`     | The URL of your discord webhook                                                                                      |
+|-------------------|----------------------------------------------------------------------------------------------------------------------|
+| `gmr_api_url`     | Do not modifiy                                                                                                       |
+| `my_id`           | Your steam id/gmr id                                                                                                 |
+| `gmr_auth_key`    | The auth key for your gmr game                                                                                       |
+| `game_name`       | The name of your game                                                                                                |
+| `gmr_discord_lut` | A sub dictionary for each user in the game where the key represents the steam/gmr id and the value is the discord id |
+4. Run with `python3 gmr_notif_discord.py`
---
+++ gmr_notif_discord.py
@@ -0,0 +1,73 @@
+import asyncio
+from datetime import datetime
+from json import loads
+from pathlib import Path
+
+from aiohttp import ClientSession
+from dateutil.parser import isoparse
+from discord import Color, Embed, Webhook
+
+config: dict[str, str] = loads(Path("config.json").read_text())
+
+SLEEP_TIME_SEC = 60
+
+def find_game(games: dict[str, dict[str, str]]) -> dict[str, str]:
+    for game in games["Games"]:
+        if game["Name"] == config["game_name"]:
+            return game
+
+
+async def main():
+    last_player: str = ""
+    first_run = True
+
+    async with ClientSession() as session:
+        webhook = Webhook.from_url(config["webhook_url"], session=session)
+
+        while True:
+            async with session.get(
+                f"{config['gmr_api_url']}/GetGamesAndPlayers?playerIDText={config['my_id']}&authKey={config['gmr_auth_key']}"
+            ) as response:
+                players_and_games: dict[str, str] = await response.json()
+            current_game = find_game(players_and_games)
+
+            if current_game is None:
+                await asyncio.sleep(SLEEP_TIME_SEC)
+                continue
+
+            current_turn = current_game["CurrentTurn"]
+
+            if first_run: # drop first message on startup to prevent spam
+                last_player = current_turn["UserId"]
+                first_run = False
+
+
+            if last_player == current_turn["UserId"]:
+                await asyncio.sleep(SLEEP_TIME_SEC)
+                continue
+
+            last_player = current_turn["UserId"]
+
+            await webhook.send(
+                embed=Embed(
+                    title=f"Game: {current_game['Name']}",
+                    description=f"<@{config['gmr_discord_lut'][str(current_turn['UserId'])]}> [it's your turn.](http://multiplayerrobot.com/Game#{current_game['GameId']})",
+                    color=Color.purple(),
+                    timestamp=datetime.now(),
+                )
+                .set_author(
+                    name="Giant Multiplayer Robot",
+                    icon_url="http://multiplayerrobot.com/Content/images/New_Unit_Mech_94.png",
+                )
+                .set_footer(text="👾")
+                .add_field(
+                    name=f"You have until `{isoparse(current_turn['Expires']).strftime('%c')}` to play your turn.",
+                    value="",
+                )
+            )
+
+            await asyncio.sleep(SLEEP_TIME_SEC)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
